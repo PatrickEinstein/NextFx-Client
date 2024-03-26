b865f55398915bb736d6784f0a17be213fdfc9f6
PatrickEinstein <mohammedola1234@gmail.com>
2024-03-26 16:13:12 +0100

    put live backend

--- utils/fetches/api.fetch.ts
+++ utils/fetches/api.fetch.ts
@@ -1,6 +1,7 @@
 import { HttpGetCallerWhole } from "../index";
 
-const baseUrl = "http://localhost:5000";
+// const baseUrl = "http://localhost:5000";
+const baseUrl = "https://fxserver.onrender.com";
 
 export const CalendarNews = async () => {
   const res = await HttpGetCallerWhole(
