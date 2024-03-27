f6d24319074452caa4afcbaf70d0a5279144dfcc
PatrickEinstein <mohammedola1234@gmail.com>
2024-03-27 14:34:33 +0100

    upda

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,11 +18,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <div className="flex w-full mt-10 gap-4 max-w-7xl mx-auto items-center justify-center">
-        {/* <div className="w-full md:w-9/12"> */}
         <New />
-        {/* <AdminBlogs /> */}
-        {/* <NewsBlogsBox /> */}
-        {/* </div> */}
         <div className="hidden md:flex md:font-bold md: text-black text-wrap w-3/12 overflow-hidden"></div>
       </div>
       <div className="flex flex-col md:flex-row max-w-7xl mx-auto px-4 md:px-0 pb-8">
