dcd0b9d0e28dcfc00c66bb2495288a578d8213ed
PatrickEinstein <mohammedola1234@gmail.com>
2024-04-16 13:35:34 +0100

    fix

--- src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ src/app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -2,10 +2,12 @@
 
 // import { useConfettiStore } from "@/hooks/use-confetti-store";
 import { cn } from "@/lib/utils";
+import { useUserStore } from "@/store";
 import MuxPlayer from "@mux/mux-player-react";
 import axios from "axios";
-import { Loader2, Lock } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { ArrowLeft, Loader2, Lock } from "lucide-react";
+import Link from "next/link";
+import { redirect, useRouter } from "next/navigation";
 import React, { useState } from "react";
 import toast, { ToastBar } from "react-hot-toast";
 
@@ -28,13 +30,15 @@ const VideoPlayer = ({
   completeOnEnd,
   title,
 }: VideoPlayerProps) => {
-
-
-
-
   const [isReady, setIsReady] = useState(false);
   const router = useRouter();
   // const confetti = useConfettiStore();
+
+  const user = useUserStore((state) => state.user);
+
+//   <Link href={`/dashboard/${user}`} className="text-blue-600">
+//   <ArrowLeft/> Back to dashboard
+//  </Link>
 
   const onEnd = async () => {
     try {
@@ -67,12 +71,17 @@ const VideoPlayer = ({
           <Loader2 className="h-8 w-8 animate-spin text-secondary" />
         </div>
       )}
+      <Link href={`/dashboard/${user}`} className="text-blue-600">
+       <ArrowLeft/> Back to dashboard
+      </Link>
+
       {isLocked && (
         <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
           <Lock className="h-8 w-8" />
           <p className="text-sm">This chapter is locked</p>
         </div>
       )}
+
       {!isLocked && (
         <MuxPlayer
           title={title}
